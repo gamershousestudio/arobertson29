d58757d70eb51dede04e27b84f412f1db3cb160f
gamershousestudio <gamershousestudio@gmail.com>
2025-09-05 13:41:40 -0400

    First program
    Hello world program for my cpp class.

---
+++ hello_world.cpp
@@ -0,0 +1,27 @@
+/* FILE NAMING NOTES
+
+File naming rules for programming:
+ - No spaces
+ - No special characters(except - and _)
+ - Must start with a letter
+ - File must end in programming language(.py, .cs, .cpp, java, .js, .html, .css, ect.)
+
+File naming norms for programming:
+ - camelCase
+ - snake_case
+ - UpperCase
+ - Should be related to task(variables, files, functions, etc.)
+
+*/
+
+#include <iostream> // Input output stream
+using namespace std; // Standard lib
+
+int main()
+{
+    // Prints 'Hello, World!' to the console
+    cout << "Hello, World!" << endl;
+
+    // Returns with error code 0 (no issues)
+    return 0;
+}
