760bb0805f4b67b2abac8430fc64493e183a3b08
gamershousestudio <gamershousestudio@gmail.com>
2025-09-09 10:31:29 -0400

    Day 2
    Added to notes.

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,19 +8,39 @@ File naming rules for programming:
 
 File naming norms for programming:
  - camelCase
- - snake_case
+ - snake_case <-CPP FILES USE THIS
  - UpperCase
  - Should be related to task(variables, files, functions, etc.)
 
 */
 
-#include <iostream> // Input output stream
-using namespace std; // Standard lib
+/* VARIABLE NOTES
 
+What is a variables:
+ - Stores things(int, float, bool, char(subset of int; uses ascii), string(list of chars), double(double storage of float), etc.)
+ - Takes up space
+ - Can use long & short to save/use more space
+ 
+Creating variables:
+ - Subset type name = value;
+ - Subset type name;
+ - Name should follow the naming norms for programming
+ - Declaration: specifying type and name
+ - Initialization: giving a variable a value
+
+*/
+
+#include <iostream> // Input output stream
+
+// Main function, code executed line by line inside
 int main()
 {
+    // Imports
+    using std::cout;
+    using std::cin;
+
     // Prints 'Hello, World!' to the console
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World!\n";
 
     // Returns with error code 0 (no issues)
     return 0;
