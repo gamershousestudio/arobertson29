f6bbfc033485c206e7e86083b3470be2e46a826d
gamershousestudio <gamershousestudio@gmail.com>
2025-10-09 13:08:00 -0400

    New mini project!
    yayyyyyyyyyyyy... (shinitai kudasai)

---
+++ Code/Homework/guess.cpp
@@ -0,0 +1,16 @@
+/*
+
+PSEUDOCODE:
+
+In main
+ - secret is randomly generated
+ - welcome text
+ - In while
+   - ran until number is guessed
+   - user enters their guess
+   - output GiveHint
+
+In GiveHint
+ - Tell user if number is too low, too high, or correct
+
+*/
