0a07c6965612ca424e239d2422888e828b26e77d
gamershousestudio <gamershousestudio@gmail.com>
2025-11-12 10:50:34 -0500

    sdfghdjtkl/
    im tired and lazy so pretend this is a cool description.

---
+++ Code/Classwork/sorting-notes.cpp
@@ -0,0 +1,79 @@
+/* NOTES
+
+Types: bubble, selection, insertion
+
+O notation:
+ - signifies speed & complexity of algorithm
+ - Bubble: O(n^2)
+ - Selection: O(n^2)
+ - Insertion O(n)
+ 
+
+*/
+
+void Bubble(int a[]);
+
+void Swap(int & a, int & b);
+
+const int length = 1000;
+const int range = 1000;
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    srand(time(NULL));
+
+    int array[length];
+
+    cout << "\nUnsorted array: \n";
+
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = rand() % range;
+        cout << " [ " << array[i] << " ] ";
+    }
+
+    Bubble(array);
+
+    cout << "\nSorted array: \n";
+
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = rand() % range;
+        cout << " [ " << array[i] << " ] ";
+    }
+
+    cout << "\n\n";
+
+    return 0;
+}
+
+void Bubble(int a[])
+{
+    bool changed = 1;
+
+    while(changed)
+    {
+        changed = false;
+
+        for(int i = 0; i < length - 1; i++)
+        {
+            if(a[i] > a[i + 1])
+            {
+                Swap(a[i], a[i + 1]);
+
+                changed = true;
+            }
+        }
+    }
+}
+
+void Swap(int & a, int & b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
